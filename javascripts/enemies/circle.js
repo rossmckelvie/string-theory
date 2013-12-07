b786730b9bb20bf14bdd0aadc5e71aca1a6c2614
Ross McKelvie <r@rossmckelvie.com>
2013-12-07 01:47:35 -0800

    randonmess of circle parameters increased, cleaner entry onto game

--- javascripts/enemies/circle.js
+++ javascripts/enemies/circle.js
@@ -4,44 +4,77 @@ Circle = Class.create(Enemy, {
     this.image = game.assets['images/circle_glow.png'];
     this.frame = 0;
 
-    this.radius = 80;
+    this.x = x;
+    this.y = y;
+
+    this.radius = this.getRandomRadius(60, 100);
     this.angle = 0;
     this.speed = 8;
     this.scoreValue = 20;
 
-    // Set y to top of sattelite distance
-    if (y - this.radius < 0) {
-      this.y = this.radius * 2;
-      this.centerY = this.radius * 2;
+    this.movedToStartPosition = false;
+    var offsetX = this.getOffsetValue((game.width / 2) - this.radius * 1.5);
+    var offsetY = this.getOffsetValue((game.height / 2) - this.radius * 1.5);
+
+    // Determine start positions and determine centers
+    // TOP LEFT (+ offsetX | + offsetY)
+    if (x === 0 && y === 0) {
+      this.angle = 0;
+      this.desireY = this.radius + offsetY;
+      this.centerY = this.radius * 2 + offsetY;
+      this.desireX = this.radius * 2 + offsetX;
+      this.centerX = this.radius * 2 + offsetX;
     }
-    else if (y + this.height + this.radius > game.height) {
-      this.y = game.height - this.height;
-      this.centerY = game.height - this.height;
+    // BOTTOM LEFT (+ offsetX | - offsetY)
+    else if (x === 0 && y > 0) {
+      this.angle = 180;
+      this.desireY = game.height - this.height - this.radius - offsetY;
+      this.centerY = game.height - this.height - offsetY;
+      this.desireX = 0 + offsetX;
+      this.centerX = this.radius * 2 + offsetX;
     }
-    else {
-      this.y = y - y;
-      this.centerY = y;
+    // TOP RIGHT (- offsetX | + offsetY)
+    else if (x > 0 && y === 0) {
+      this.angle = 0;
+      this.desireY = this.radius + offsetY;
+      this.centerY = this.radius * 2 + offsetY;
+      this.desireX = game.width - this.width - offsetX;
+      this.centerX = game.width - this.width - offsetX;
+    }
+    // BOTTOM RIGHT (- offsetX | - offsetY)
+    else if (x > 0 && y > 0) {
+      this.angle = 180;
+      this.desireY = game.height - this.height - this.radius - offsetY;
+      this.centerY = game.height - this.height - offsetY;
+      this.desireX = game.width - this.width - (this.radius * 2) - offsetX;
+      this.centerX = game.width - this.width - offsetX;
     }
 
-    // Set x so that it doesn't overflow game
-    if (x - (this.radius * 2) < 0) {
-      this.x = this.radius * 4;
-      this.centerX = this.radius * 2;
-    }
-    else if (x + this.width > game.width) {
-      this.x = game.width - this.width;
-      this.centerX = game.width - this.width;
-    }
-    else {
-      this.x = x;
-      this.centerX = x - this.radius;
-    }
-
-    console.log("start: (" + this.x + ", " + this.y + ")");
+    // Set vectors for moving into position
+    this.xVector = this.desireX - this.x;
+    this.yVector = this.desireY - this.y;
+    this.startMoveAngle = Math.atan2(this.yVector, this.xVector);
   },
 
   onenterframe: function() {
     this.collisionDetect();
+
+    // Move into position if needed
+    if (!this.movedToStartPosition) {
+      this.xSpeed = this.speed * Math.cos(this.startMoveAngle);
+      this.ySpeed = this.speed * Math.sin(this.startMoveAngle);
+
+      if (this.xSpeed === 0 && this.ySpeed === 0)
+        this.xSpeed = 1;
+
+      this.x += this.xSpeed;
+      this.y += this.ySpeed;
+
+      var diff = Math.abs(this.desireX - this.x) + Math.abs(this.desireY - this.y);
+      if (diff < this.speed)
+        this.movedToStartPosition = true;
+      else return;
+    }
 
     // Enrage after some time
     if (this.age > 500 && this.frame === 0) {
@@ -83,5 +116,13 @@ Circle = Class.create(Enemy, {
 
     this.x += this.xSpeed;
     this.y += this.ySpeed;
+  },
+
+  getOffsetValue: function(maxOffset) {
+    return Math.floor(Math.random() * maxOffset);
+  },
+
+  getRandomRadius: function(shortest, longest) {
+    return Math.floor(Math.random() * (longest - shortest + 1) + shortest);
   }
 });
