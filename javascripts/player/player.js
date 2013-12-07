eaafd3c12f34a3d348caa80c6e1834a943c75691
Ross McKelvie <r@rossmckelvie.com>
2013-12-06 17:43:00 -0800

    readability

--- javascripts/player/player.js
+++ javascripts/player/player.js
@@ -1,65 +1,64 @@
-Player = Class.create(Sprite, // We extend the Sprite class
-{
-    initialize: function() { //initialization
-        Sprite.call(this, 192, 192); //initialization of the Sprite object
-        this.image = game.assets['images/nwomatri.png'];
-        this.x = (game.width / 2) - (this.width / 2);;
-        this.y = (game.height / 2) - (this.height / 2);;
-        this.frame = 7;
-		this.increment = 1;
-		this.numLasers = 0;
-		this.moveSpeed = 5;
-		this.omega = 2;
-    },
-	
-	onenterframe: function () {
-        if (this.age % 4 === 0) {
-			this.frame += this.increment;
-        }
-            
-        if (this.frame >=11) {
-			this.increment = -1;
-        }
-			
-		if (this.frame <= 7) {
-			this.increment = 1;
-		}
-		
-		this.rotate(this.omega);
-		
-		if (game.input.left && !game.input.right) {
-			this.x -= this.moveSpeed;
-		}
-			
-		else if (game.input.right && !game.input.left) {
-			this.x += this.moveSpeed;
-		}
-			
-		if (game.input.up && !game.input.down) {
-			this.y -= this.moveSpeed;
-		}
-			
-		else if (game.input.down && !game.input.up) {
-			this.y += this.moveSpeed;
-		}
-			
-		if (this.x < -this.width/2) {
-			this.x = -this.width/2;
-		}
-		
-		if (this.x > game.width - this.width/2) {
-			this.x = game.width - this.width/2;
-		}
-		
-		if (this.y < -this.height/2) {
-			this.y = -this.height/2;
-		}
-		
-		if (this.y > game.height - this.height/2) {
-			this.y = game.height - this.height/2;
-		}
-			
-			
-	},
-	
+Player = Class.create(Sprite, {
+  initialize: function() { //initialization
+    Sprite.call(this, 192, 192);
+    this.image = game.assets['images/nwomatri.png'];
+
+    this.x = (game.width / 2) - (this.width / 2);;
+    this.y = (game.height / 2) - (this.height / 2);;
+
+    this.frame = 7;
+    this.increment = 1;
+    this.numLasers = 0;
+    this.moveSpeed = 5;
+    this.omega = 2;
+  },
+
+  onenterframe: function () {
+    if (this.age % 4 === 0) {
+      this.frame += this.increment;
+    }
+
+    if (this.frame >=11) {
+      this.increment = -1;
+    }
+
+    if (this.frame <= 7) {
+      this.increment = 1;
+    }
+
+    this.rotate(this.omega);
+
+    if (game.input.left && !game.input.right) {
+      this.x -= this.moveSpeed;
+    }
+
+    else if (game.input.right && !game.input.left) {
+      this.x += this.moveSpeed;
+    }
+
+    if (game.input.up && !game.input.down) {
+      this.y -= this.moveSpeed;
+    }
+
+    else if (game.input.down && !game.input.up) {
+      this.y += this.moveSpeed;
+    }
+
+    if (this.x < -this.width/2) {
+      this.x = -this.width/2;
+    }
+
+    if (this.x > game.width - this.width/2) {
+      this.x = game.width - this.width/2;
+    }
+
+    if (this.y < -this.height/2) {
+      this.y = -this.height/2;
+    }
+
+    if (this.y > game.height - this.height/2) {
+      this.y = game.height - this.height/2;
+    }
+  }
+
 });
--- javascripts/player/weapons.js
+++ javascripts/player/weapons.js
@@ -1,57 +1,57 @@
-PlayerShoot = Class.create(Sprite, // We extend the Sprite class
-{
-    initialize: function(x, y, mx, my) { //initialization
-        Sprite.call(this, 32, 32); //initialization of the Sprite object
-        this.image = game.assets['images/balls.png'];
-        this.x = (x) + newPlayer.width/2 - (this.width / 2);
-        this.y = (y) + newPlayer.height/2 - (this.height / 2);
-        this.frame = 0;
-		this.increment = 1;
-		this.xVector = mx - (x + newPlayer.width/2) ;
-		this.yVector = my - (y + newPlayer.height/2);
-		this.Angle = Math.atan2(this.yVector, this.xVector);
-		this.Spd = 10;
-		this.xSpd = this.Spd * Math.cos(this.Angle);
-		this.ySpd = this.Spd * Math.sin(this.Angle);
-		
-		this.dead = 0;
-		
-		if (this.xSpd === 0 && this.ySpd === 0) {
-			this.xSpd = 1;
-		}
-		
-		lasers.push(this);
-		
-    },
-	
-	onenterframe: function () {
-		
-		if (this.age % 2 === 0) {
-			this.frame += this.increment;
-        }
-            
-		if (this.frame >=2) {
-			this.increment = -1;
-        }
-			
-		if (this.frame <= 0) {
-			this.increment = 1;
-		}
+PlayerShoot = Class.create(Sprite, {
 
-		if (this.x + this.width < 0 ||
-			this.x - this.width > game.width ||
-			this.y + this.height < 0 ||
-			this.y - this.height > game.height ||
-			(this.dead)){
-			
-			newPlayer.numLasers --;
-			lasers.pop(this);
-			this.parentNode.removeChild(this);
-			this.destroy;
-		}
-		
-		this.x = this.x + this.xSpd;
-		this.y = this.y + this.ySpd;
-		
-	}
+  initialize: function(x, y, mx, my) { //initialization
+    Sprite.call(this, 32, 32);
+    this.image = game.assets['images/balls.png'];
+
+    this.x = (x) + newPlayer.width/2 - (this.width / 2);
+    this.y = (y) + newPlayer.height/2 - (this.height / 2);
+    this.frame = 0;
+    this.increment = 1;
+
+    this.xVector = mx - (x + newPlayer.width/2) ;
+    this.yVector = my - (y + newPlayer.height/2);
+    this.Angle = Math.atan2(this.yVector, this.xVector);
+
+    this.Spd = 10;
+    this.xSpd = this.Spd * Math.cos(this.Angle);
+    this.ySpd = this.Spd * Math.sin(this.Angle);
+
+    this.dead = 0;
+
+    if (this.xSpd === 0 && this.ySpd === 0) {
+      this.xSpd = 1;
+    }
+
+    lasers.push(this);
+  },
+
+  onenterframe: function () {
+    if (this.age % 2 === 0) {
+      this.frame += this.increment;
+    }
+
+    if (this.frame >=2) {
+      this.increment = -1;
+    }
+
+    if (this.frame <= 0) {
+      this.increment = 1;
+    }
+
+    if (this.x + this.width < 0 ||
+        this.x - this.width > game.width ||
+        this.y + this.height < 0 ||
+        this.y - this.height > game.height ||
+        (this.dead)) {
+
+      newPlayer.numLasers --;
+      lasers.pop(this);
+      this.parentNode.removeChild(this);
+      this.destroy;
+    }
+
+    this.x = this.x + this.xSpd;
+    this.y = this.y + this.ySpd;
+  }
 });
